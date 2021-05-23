169829e5aef02c334519bf38160393f5730201a4
teiavava <teia_vava@yahoo.com>
2021-05-23 18:32:42 +0300

    kong stack instead of stack kong

--- deploy-kong.sh
+++ deploy-kong.sh
@@ -2,6 +2,6 @@ docker swarm leave --force
 
 docker swarm init --advertise-addr $(ifconfig eth0 | awk '/inet /{print $2}' | cut -f2 -d':')
 
-docker stack deploy -c stack-kong.yml idp
+docker stack deploy -c kong-stack.yml idp
 docker service ls
 # docker service logs idp_io-service
